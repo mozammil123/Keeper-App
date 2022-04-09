a9c480a5717b28c7c4c78525d2b9b8e0d63364bc
moza123mmil@gmail.com <moza123mmil@gmail.com>
2022-04-09 12:49:56 +0530

    Updates

---
+++ precache-manifest.548c611de9e7ebc826f86ad626a081d4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c6845a7ab51cc4ffbcd86104f83ffc",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "23a1648a3522cd15f1b0",
+    "url": "/Keeper-App/static/js/2.81e768ca.chunk.js"
+  },
+  {
+    "revision": "c1009a8b4e1f11b7c7ac",
+    "url": "/Keeper-App/static/js/main.96c96f72.chunk.js"
+  },
+  {
+    "revision": "660c3a2d9cc725da8ecd",
+    "url": "/Keeper-App/static/js/runtime-main.78711218.js"
+  }
+]);
